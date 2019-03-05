476adfe42907bd5584cf2b8e99236e9883f65c21
Merjen Charyyeva <merjen.chary@gmail.com>
2019-03-05 18:09:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -20,8 +20,9 @@ end
 
 def get_user_input
   # code #get_user_input here
-   gets.strip
-   #removes whitespace around the sting  
+   gets.strip.chomp
+   #.strip removes whitespace around the sting  
+   #.chomp 
 end
 
 def end_game
